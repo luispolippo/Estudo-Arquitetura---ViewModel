a3ca6541ffbe0f2bdab250d294596d9e854cdbe8
Luis Guilherme Polippo <luispolippo@hotmail.com>
2021-03-09 11:46:24 -0300

    created the code´s logic

--- app/src/main/java/com/polippo/vmodel/MainActivity.kt
+++ app/src/main/java/com/polippo/vmodel/MainActivity.kt
@@ -2,10 +2,54 @@ package com.polippo.vmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var etContador: EditText
+    lateinit var btnDados: Button
+    lateinit var btnMostrar: Button
+
+    var contador: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initDados()
+        initContador()
+        initClick()
+        validaContador()
+    }
+
+    private fun validaContador() {
+        if(contador > 5){
+            contador = 0
+        }
+    }
+
+    private fun initClick() {
+        btnDados.setOnClickListener {
+            contador++
+            validaContador()
+            initContador()
+        }
+        btnMostrar.setOnClickListener {
+            Toast.makeText(this, "Contador: ${contador.toString()}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initContador() {
+        etContador.setText(contador.toString())
+    }
+
+    private fun initDados() {
+
+        etContador = findViewById(R.id.et_contador)
+        btnDados = findViewById(R.id.btn_dados)
+        btnMostrar = findViewById(R.id.btn_mostrar)
+
     }
 }
