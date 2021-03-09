cc8181de1c65d7c5f528392ac34279b99a30845b
Luis Guilherme Polippo <luispolippo@hotmail.com>
2021-03-09 17:34:44 -0300

    created MainViewModel class

--- app/src/main/java/com/polippo/vmodel/MainActivity.kt
+++ app/src/main/java/com/polippo/vmodel/MainActivity.kt
@@ -2,9 +2,12 @@ package com.polippo.vmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,44 +15,41 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnDados: Button
     lateinit var btnMostrar: Button
 
-    var contador: Int = 0
+     var mViewModel = MainViewModel()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initDados()
-        initContador()
         initClick()
-        validaContador()
-    }
 
-    private fun validaContador() {
-        if(contador > 5){
-            contador = 0
-        }
-    }
-
-    private fun initClick() {
-        btnDados.setOnClickListener {
-            contador++
-            validaContador()
-            initContador()
-        }
-        btnMostrar.setOnClickListener {
-            Toast.makeText(this, "Contador: ${contador.toString()}", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun initContador() {
-        etContador.setText(contador.toString())
     }
 
     private fun initDados() {
+        mViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         etContador = findViewById(R.id.et_contador)
         btnDados = findViewById(R.id.btn_dados)
         btnMostrar = findViewById(R.id.btn_mostrar)
 
+        mViewModel.mContador.observe(this, Observer { valor ->
+            etContador.setText(valor)
+        })
+
     }
+
+
+    private fun initClick() {
+        btnDados.setOnClickListener {
+            mViewModel.contador()
+
+        }
+        btnMostrar.setOnClickListener {
+            Toast.makeText(this, "Contador: ${mViewModel.mContador.value}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
